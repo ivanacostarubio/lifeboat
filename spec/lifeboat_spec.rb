c64ee7dd7d42230108ca67e2c2e89fc462ebb155
Ivan Acosta-Rubio <ivan@bakedweb.net>
2011-04-28 05:40:18 +0800

    Adding a reading cue method Adding the name of the model for queue creation

--- spec/lifeboat_spec.rb
+++ spec/lifeboat_spec.rb
@@ -1,8 +1,5 @@
 require 'rubygems'
-require 'active_record'
 require 'rspec'
-require 'right_aws'
-
 require 'lifeboat'
 
 config = YAML::load(IO.read(File.dirname(__FILE__) + '/../support/database.yml'))
@@ -34,6 +31,16 @@ class Fake < ActiveRecord::Base
   include LifeBoat
 end
 
+
+class Helper
+def self.clean_all_queues
+   @sqs = RightAws::SqsGen2.new(Credentials.key,Credentials.secret)
+   @sqs.queues.each do |queue|
+     @sqs.queue(queue.name.to_s).clear
+   end
+end
+end
+
 describe "An simple object " do
   it "raises for not having callbacks" do
     lambda{ class BadModel ; include LifeBoat ; end }.should raise_error
@@ -61,28 +68,28 @@ describe FakeModel, " We hook into callbacks to send the messages" do
   end
 end
 
+describe LifeBoat, " Create" do
+  before(:each) do
+    Helper.clean_all_queues
+  end
+
+  it "the message it creates contains the attributes ob the object as json" do
+    f = Fake.create(:name => "ivan")
+    q = LifeBoat.read_queue("create_fake")
+    q[0].body.should == f.attributes.to_json
+  end
+end
+
 describe LifeBoat  do
 
   before(:each) do
-     @sqs = RightAws::SqsGen2.new(Credentials.key,Credentials.secret)
-     @sqs.queue('fake').clear
-     @sqs.queue('fake_model').clear
+    Helper.clean_all_queues
   end
 
   it "reads messages from a cue" do
-    pending
-  end
-
-  describe " CREATING" do
-      it "creates a cue" do
-        f = Fake.create(:name => "ivan")
-        q = @sqs.queue("create").receive_messages
-        q.size.should == 1
-      end
-
-      it "the message it creates contains the attributes ob the object as json" do
-        pending
-      end
+    Fake.create(:name => "ivan")
+    messages = LifeBoat.read_queue("create_fake")
+    messages.size.should == 1
   end
 
   it "Raises when no credentials are given" do
