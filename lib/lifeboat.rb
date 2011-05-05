61ea0646ed7e3f90f7cc5a49e40fd147be1ff17c
Ivan Acosta-Rubio <ivan@bakedweb.net>
2011-05-05 14:15:39 -0400

    Refactor into Extending method for ActiveRecord::Base Test for XML serialization

--- lib/lifeboat.rb
+++ lib/lifeboat.rb
@@ -22,7 +22,6 @@ class AWS
 end
 
 
-
 class Credentials
   def initialize
 # TRIED USING THE INITIALIZE FOR THOSE YAML LOADING DOWN THERE
@@ -45,14 +44,6 @@ end
 
 
 module LifeBoat
-  def self.included(base)
-    raise "Object Lacks Proper Callbacks" unless base.respond_to? :after_create
-    base.class_eval do
-      after_create :create_lifeboat
-      after_destroy :destroy_lifeboat
-      after_update :update_lifeboat
-    end
-  end
 
   def self.read_queue(name)
     #TODO EXTRAT OUT THE @CUE INTO HIGHER LEVEL
@@ -60,20 +51,40 @@ module LifeBoat
     return @cue.queue(name).receive_messages
   end
 
-  def after_initialize
-    @cue = RightAws::SqsGen2.new(Credentials.key, Credentials.secret)
+  module ActiveRecord
+    def has_lifeboat
+      include Queues
+    end
   end
 
-  [:create, :update, :destroy ].each do |action|
-    define_method(action.to_s + "_lifeboat") do
-      begin
-        queue_name = action.to_s+"_"+ self.class.to_s.downcase + "_" + RAILS_ENV
-        q = RightAws::SqsGen2::Queue.create(@cue, queue_name, true)
-        q.send_message(self.attributes.to_json)
-      rescue RightAws::AwsError => e
-        puts "LifeBoat RightAws::AwsError TIMEOUT"
-        puts e
+
+  module Queues
+    def self.included(base)
+      raise "Object Lacks Proper Callbacks" unless base.respond_to? :after_create
+      base.class_eval do
+        after_create :create_lifeboat
+        after_destroy :destroy_lifeboat
+        after_update :update_lifeboat
+      end
+    end
+
+    def after_initialize
+      @cue = RightAws::SqsGen2.new(Credentials.key, Credentials.secret)
+    end
+
+    [:create, :update, :destroy ].each do |action|
+      define_method(action.to_s + "_lifeboat") do
+        begin
+          queue_name = action.to_s+"_"+ self.class.to_s.downcase + "_" + RAILS_ENV
+          q = RightAws::SqsGen2::Queue.create(@cue, queue_name, true)
+          q.send_message(self.attributes.to_json)
+        rescue RightAws::AwsError => e
+          puts "LifeBoat RightAws::AwsError TIMEOUT"
+          puts e
+        end
       end
     end
   end
 end
+
+ActiveRecord::Base.extend LifeBoat::ActiveRecord
